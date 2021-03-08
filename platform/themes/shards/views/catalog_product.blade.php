98e844ff3899a366dc30c1071d83d933c2f80c4c
Trung <trung.nguyen@comartek.vn>
2021-03-08 09:30:19 +0700

    update

--- platform/themes/shards/views/catalog_product.blade.php
+++ platform/themes/shards/views/catalog_product.blade.php
@@ -92,10 +92,16 @@ $relatedProduct = get_related_products($product->id,10);
                                     <hr>
                                     <p class="PriceProHome">
                                         <?php if ($Pro->discount_price) : ?>
-                                        <span class="font-18 product-price">$ {{ ($Pro->discount_price) }}</span>
-                                        <del class="font-14"> $ {{ $Pro->price }}</del>
+                                        <span class="font-18 product-price">{{ ($Pro->discount_price) }} VND</span>
+                                        <del class="font-14">{{ $Pro->price }} VND</del>
                                         <?php else : ?>
-                                        <span class="font-18 product-price">$ {{ ($Pro->price) }}</span>
+                                        @if($Pro->price > 0)
+                                        <span class="font-18 product-price">{{ ($Pro->price) }} VND</span>
+                                        @else
+                                        <a href="tel:{{ theme_option('phone') }}" class="product-price">
+                                            Liên hệ
+                                        </a>
+                                        @endif
                                         <?php endif ?>
                                     </p>
                                 </div>
